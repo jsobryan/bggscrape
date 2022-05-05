1f2245fc837335f66ab106c6ead5b9c859078ba9
jsobryan <jsobryan@gmail.com>
2022-05-05 10:58:56 -0500

    first commit

---
+++ bgg.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import requests
+import json
+
+bgg_info = []
+base_url = "https://authority.pub/negative-adjectives/"
+
+response = requests.get(base_url)
+soup = BeautifulSoup(response.text, "html.parser")
+games = soup.find_all('p.strong')
+for game in games:
+    names = game.find('a')
+    bgg_info.append(names.get_text())
+
+for i, item in enumerate(bgg_info,1):
+    print(i, '. ' + item, sep='',end='\n')
+
+with open('negwords.json', 'w') as json_file:
+    json.dump(bgg_info, json_file)
