a0cffbc74359f4970f5145ead6c3286005de0d6b
jsobryan <jsobryan@gmail.com>
2022-05-05 12:59:37 -0500

    corrected

--- bgg.py
+++ bgg.py
@@ -3,11 +3,11 @@ import requests
 import json
 
 bgg_info = []
-base_url = "https://authority.pub/negative-adjectives/"
+base_url = "https://boardgamegeek.com/browse/boardgame"
 
 response = requests.get(base_url)
 soup = BeautifulSoup(response.text, "html.parser")
-games = soup.find_all('p.strong')
+games = soup.find_all('td', {'class': 'collection_objectname'} )
 for game in games:
     names = game.find('a')
     bgg_info.append(names.get_text())
